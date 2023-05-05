b68d42848bf9b533bc1de0dd6eb76f32104bb2f7
Johny Kidd <kidnixt@gmail.com>
2023-05-05 10:11:02 -0300

    class finished

--- src/components/Persona.js
+++ src/components/Persona.js
@@ -1,12 +1,12 @@
 //props, mecanismo que permite pasar datos de un componente padre a un componente hijo
-const Persona = ({nombre}) => {
+const Persona = ({nombre, edad}) => {
   //destructuring va con llaves
   //tambien se puede hacer en los parametros de la funcion
   //ya que en react no se utilizan mucho las variables
   //let {nombre} = props
   return (
     <div>Componente dinamico
-        <h2>Hola soy {nombre}</h2>
+        <h2>Hola soy {nombre} y tengo {edad}</h2>
     </div>
 
   )
