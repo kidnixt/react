0955c272d3b8b8c89c8a3e71c3d983104d475b07
Johny Kidd <kidnixt@gmail.com>
2023-05-05 09:33:53 -0300

    map arrow function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Persona from './components/Persona';
 import './App.css';
 
 function App() {
+
+  let nombres = ["nombres", "desdeArray", "usuario1" , "usuario2", "usuario3"]
+
   return (
     //codigo jsx, se debe tener un solo elemento padre
     <div className="App">
@@ -15,6 +18,10 @@ function App() {
           <br />
           <Persona nombre = "johny" />
           <Persona nombre = "fiumba" />
+          <hr />
+          {nombres.map((nombre) => {
+            return <Persona nombre = {nombre}/>
+          })}
         </p>
         <a
           className="App-link"
--- src/components/Persona.js
+++ src/components/Persona.js
@@ -1,8 +1,12 @@
 //props, mecanismo que permite pasar datos de un componente padre a un componente hijo
-const Persona = (props) => {
+const Persona = ({nombre}) => {
+  //destructuring va con llaves
+  //tambien se puede hacer en los parametros de la funcion
+  //ya que en react no se utilizan mucho las variables
+  //let {nombre} = props
   return (
     <div>Componente dinamico
-        <h2>Hola soy {props.nombre}</h2>
+        <h2>Hola soy {nombre}</h2>
     </div>
 
   )
