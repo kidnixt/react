61bc9179b551df4991f809250676b817ab65350d
Johny Kidd <kidnixt@gmail.com>
2023-05-05 10:00:23 -0300

    spreading map

--- src/App.js
+++ src/App.js
@@ -5,24 +5,32 @@ import './App.css';
 
 function App() {
 
-  let nombres = ["nombres", "desdeArray", "usuario1" , "usuario2", "usuario3"]
+  let personas = [
+    {id:1, nombre: "johny", edad: 20},
+    {id:2, nombre: "fiumba", edad: 30},
+    {id:3, nombre: "jose", edad: 40}]
 
   return (
     //codigo jsx, se debe tener un solo elemento padre
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <div>
           aberga
           <Texto />
           <br />
           <Persona nombre = "johny" />
           <Persona nombre = "fiumba" />
           <hr />
-          {nombres.map((nombre) => {
-            return <Persona nombre = {nombre}/>
-          })}
-        </p>
+          {/*nombres.map((nombre, index) => {
+            console.log(nombre + " " + index)
+            return <Persona key={index} nombre={nombre} />
+          })*/}
+
+          {personas.map((persona) => {return <Persona key={persona.id} {...persona} />})}
+
+          
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
